59c43b9df32c5fa4ddebc944db711d9c1a1ecaee
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-02 17:55:56 +0200

    Create container.go

---
+++ server/block/container.go
@@ -0,0 +1,59 @@
+ 
+
+ package block 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/block/cube" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item/inventory" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/world" 
+
+ ) 
+
+  
+
+ // ContainerViewer represents a viewer that is able to view a container and its inventory. 
+
+ type ContainerViewer interface { 
+
+         // ViewSlotChange views a change of a single slot in the inventory, in which the item was changed to the 
+
+         // new item passed. 
+
+         ViewSlotChange(slot int, newItem item.Stack) 
+
+ } 
+
+  
+
+ // ContainerOpener represents an entity that is able to open a container. 
+
+ type ContainerOpener interface { 
+
+         // OpenBlockContainer opens a block container at the position passed. 
+
+         OpenBlockContainer(pos cube.Pos) 
+
+ } 
+
+  
+
+ // Container represents a container of items, typically a block such as a chest. Containers may have their 
+
+ // inventory opened by viewers. 
+
+ type Container interface { 
+
+         AddViewer(v ContainerViewer, w *world.World, pos cube.Pos) 
+
+         RemoveViewer(v ContainerViewer, w *world.World, pos cube.Pos) 
+
+         Inventory() *inventory.Inventory 
+
+ }
