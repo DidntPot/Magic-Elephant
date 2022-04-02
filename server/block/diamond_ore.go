9042ddfa081286e9636e78e68ebe433e62addd49
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-02 18:29:33 +0200

    Create diamond_ore.go

---
+++ server/block/diamond_ore.go
@@ -0,0 +1,67 @@
+ 
+
+ package block 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+ ) 
+
+  
+
+ // DiamondOre is a rare ore that generates underground. 
+
+ type DiamondOre struct { 
+
+         solid 
+
+         bassDrum 
+
+  
+
+         // Type is the type of diamond ore. 
+
+         Type OreType 
+
+ } 
+
+  
+
+ // BreakInfo ... 
+
+ func (d DiamondOre) BreakInfo() BreakInfo { 
+
+         i := newBreakInfo(d.Type.Hardness(), func(t item.Tool) bool { 
+
+                 return t.ToolType() == item.TypePickaxe && t.HarvestLevel() >= item.ToolTierIron.HarvestLevel 
+
+         }, pickaxeEffective, silkTouchOneOf(item.Diamond{}, d)) 
+
+         i.XPDrops = XPDropRange{3, 7} 
+
+         return i 
+
+ } 
+
+  
+
+ // EncodeItem ... 
+
+ func (d DiamondOre) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:" + d.Type.Prefix() + "diamond_ore", 0 
+
+ } 
+
+  
+
+ // EncodeBlock ... 
+
+ func (d DiamondOre) EncodeBlock() (string, map[string]any) { 
+
+         return "minecraft:" + d.Type.Prefix() + "diamond_ore", nil 
+
+ }
