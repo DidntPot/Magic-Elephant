cbb2845aa56125fbdaf903601699bb0714fdb193
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-03 18:36:38 +0200

    Update ladder.go

---
+++ server/block/model/ladder.go
@@ -0,0 +1,49 @@
+ 
+
+ package model 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/block/cube" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/entity/physics" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/world" 
+
+         "github.com/go-gl/mathgl/mgl64" 
+
+ ) 
+
+  
+
+ // Ladder is the model for a ladder block. 
+
+ type Ladder struct { 
+
+         // Facing is the side opposite to the block the Ladder is currently attached to. 
+
+         Facing cube.Direction 
+
+ } 
+
+  
+
+ // AABB returns one physics.AABB that depends on the facing direction of the Ladder. 
+
+ func (l Ladder) AABB(cube.Pos, *world.World) []physics.AABB { 
+
+         return []physics.AABB{physics.NewAABB(mgl64.Vec3{}, mgl64.Vec3{1, 1, 1}).ExtendTowards(l.Facing.Face(), -0.8125)} 
+
+ } 
+
+  
+
+ // FaceSolid always returns false. 
+
+ func (l Ladder) FaceSolid(cube.Pos, cube.Face, *world.World) bool { 
+
+         return false 
+
+ }
