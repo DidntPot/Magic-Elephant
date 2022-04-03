d3833a84af9184e9c06ac64308288defc1f5fd0d
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-03 18:54:02 +0200

    Create tilled_grass.go

---
+++ server/block/model/tilled_grass.go
@@ -0,0 +1,45 @@
+ 
+
+ package model 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/block/cube" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/entity/physics" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/world" 
+
+         "github.com/go-gl/mathgl/mgl64" 
+
+ ) 
+
+  
+
+ // TilledGrass is a model used for grass that has been tilled in some way, such as dirt paths and farmland. 
+
+ type TilledGrass struct{} 
+
+  
+
+ // AABB returns a physics.AABB that spans an entire block. 
+
+ func (TilledGrass) AABB(cube.Pos, *world.World) []physics.AABB { 
+
+         // TODO: Make the max Y value 0.9375 once https://bugs.mojang.com/browse/MCPE-12109 gets fixed. 
+
+         return []physics.AABB{physics.NewAABB(mgl64.Vec3{}, mgl64.Vec3{1, 1, 1})} 
+
+ } 
+
+  
+
+ // FaceSolid always returns true. 
+
+ func (TilledGrass) FaceSolid(cube.Pos, cube.Face, *world.World) bool { 
+
+         return true 
+
+ }
