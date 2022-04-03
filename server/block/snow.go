9fa958ecab04e9060d3a3ff3746b872aba71c6bb
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-03 14:15:26 +0200

    Create snow.go

---
+++ server/block/snow.go
@@ -0,0 +1,47 @@
+ 
+
+ package block 
+
+  
+
+ import "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+  
+
+ // Snow is a full-sized block of snow. 
+
+ type Snow struct { 
+
+         solid 
+
+ } 
+
+  
+
+ // BreakInfo ... 
+
+ func (s Snow) BreakInfo() BreakInfo { 
+
+         return newBreakInfo(0.2, alwaysHarvestable, shovelEffective, silkTouchDrop(item.NewStack(item.Snowball{}, 4), item.NewStack(s, 1))) 
+
+ } 
+
+  
+
+ // EncodeItem ... 
+
+ func (Snow) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:snow", 0 
+
+ } 
+
+  
+
+ // EncodeBlock ... 
+
+ func (Snow) EncodeBlock() (string, map[string]any) { 
+
+         return "minecraft:snow", nil 
+
+ }
