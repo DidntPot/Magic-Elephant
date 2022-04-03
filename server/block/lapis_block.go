905f231990624062849407a85d01e5db1fd6f3b9
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-03 10:51:31 +0200

    Create lapis_block.go

---
+++ server/block/lapis_block.go
@@ -0,0 +1,55 @@
+ 
+
+ package block 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+ ) 
+
+  
+
+ // LapisBlock is a decorative mineral block that is crafted from lapis lazuli. 
+
+ type LapisBlock struct { 
+
+         solid 
+
+ } 
+
+  
+
+ // BreakInfo ... 
+
+ func (l LapisBlock) BreakInfo() BreakInfo { 
+
+         return newBreakInfo(3, func(t item.Tool) bool { 
+
+                 return t.ToolType() == item.TypePickaxe && t.HarvestLevel() >= item.ToolTierStone.HarvestLevel 
+
+         }, pickaxeEffective, oneOf(l)) 
+
+ } 
+
+  
+
+ // EncodeItem ... 
+
+ func (LapisBlock) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:lapis_block", 0 
+
+ } 
+
+  
+
+ // EncodeBlock ... 
+
+ func (LapisBlock) EncodeBlock() (string, map[string]any) { 
+
+         return "minecraft:lapis_block", nil 
+
+ }
