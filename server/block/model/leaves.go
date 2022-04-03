71599a93a86023138b7dbb96c9cc1f1ef8eaf051
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-03 18:39:37 +0200

    Create leaves.go

---
+++ server/block/model/leaves.go
@@ -0,0 +1,45 @@
+ 
+
+ package model 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/block/cube" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/entity/physics" 
+
+         "github.com/Pocketminer92/magic-alpaca/server/world" 
+
+         "github.com/go-gl/mathgl/mgl64" 
+
+ ) 
+
+  
+
+ // Leaves is a model for leaves-like blocks. These blocks have a full collision box, but none of their faces 
+
+ // are solid. 
+
+ type Leaves struct{} 
+
+  
+
+ // AABB returns a physics.AABB that spans a full block. 
+
+ func (Leaves) AABB(cube.Pos, *world.World) []physics.AABB { 
+
+         return []physics.AABB{physics.NewAABB(mgl64.Vec3{}, mgl64.Vec3{1, 1, 1})} 
+
+ } 
+
+  
+
+ // FaceSolid always returns false. 
+
+ func (Leaves) FaceSolid(cube.Pos, cube.Face, *world.World) bool { 
+
+         return false 
+
+ }
