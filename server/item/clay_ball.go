4324e70f433f3c3dc009e2ca6d98ed2e73a4c05c
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-07 11:44:35 +0200

    Dinge

---
+++ server/item/clay_ball.go
@@ -0,0 +1,19 @@
+ 
+
+ package item 
+
+  
+
+ // ClayBall is obtained from mining clay blocks 
+
+ type ClayBall struct{} 
+
+  
+
+ // EncodeItem ... 
+
+ func (ClayBall) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:clay_ball", 0 
+
+ }
