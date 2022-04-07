b09707d4b7f7c2212a3fcde03c6879ccf6303ca5
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-07 11:37:42 +0200

    Update bread.go

---
+++ server/item/bread.go
@@ -0,0 +1,39 @@
+ 
+
+ package item 
+
+  
+
+ import "github.com/PocketMiner92/magic-alpaca/server/world" 
+
+  
+
+ // Bread is a food item that can be eaten by the player. 
+
+ type Bread struct { 
+
+         defaultFood 
+
+ } 
+
+  
+
+ // Consume ... 
+
+ func (Bread) Consume(_ *world.World, c Consumer) Stack { 
+
+         c.Saturate(5, 6) 
+
+         return Stack{} 
+
+ } 
+
+  
+
+ // EncodeItem ... 
+
+ func (Bread) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:bread", 0 
+
+ }
