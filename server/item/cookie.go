4e188c9231505c854b84aa7c8ad2e2a6bf1f0c4d
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-07 11:49:37 +0200

    Haha Nika ist da!

---
+++ server/item/cookie.go
@@ -0,0 +1,39 @@
+ 
+
+ package item 
+
+  
+
+ import "github.com/PocketMiner92/magic-alpaca/server/world" 
+
+  
+
+ // Cookie is a food item that can be obtained in large quantities, but do not restore hunger or saturation significantly. 
+
+ type Cookie struct { 
+
+         defaultFood 
+
+ } 
+
+  
+
+ // Consume ... 
+
+ func (Cookie) Consume(_ *world.World, c Consumer) Stack { 
+
+         c.Saturate(2, 0.4) 
+
+         return Stack{} 
+
+ } 
+
+  
+
+ // EncodeItem ... 
+
+ func (Cookie) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:cookie", 0 
+
+ }
