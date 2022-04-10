88c79aade8124f8ce9576d07813f224e6a3a1047
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 17:07:37 +0200

    Create aqua_affinity.go

---
+++ server/item/enchantment/aqua_affinity.go
@@ -0,0 +1,49 @@
+ 
+
+ package enchantment 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+ ) 
+
+  
+
+ // AquaAffinity is a helmet enchantment that increases underwater mining speed. 
+
+ type AquaAffinity struct{} 
+
+  
+
+ // Name ... 
+
+ func (e AquaAffinity) Name() string { 
+
+         return "Aqua Affinity" 
+
+ } 
+
+  
+
+ // MaxLevel ... 
+
+ func (e AquaAffinity) MaxLevel() int { 
+
+         return 1 
+
+ } 
+
+  
+
+ // CompatibleWith ... 
+
+ func (e AquaAffinity) CompatibleWith(s item.Stack) bool { 
+
+         h, ok := s.Item().(item.HelmetType) 
+
+         return ok && h.Helmet() 
+
+ }
