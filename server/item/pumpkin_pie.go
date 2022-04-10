4232c9d5a83927d9543d8b3219edb38982203704
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 08:28:29 +0200

    Create pumpkin_pie.go

---
+++ server/item/pumpkin_pie.go
@@ -0,0 +1,39 @@
+ 
+
+ package item 
+
+  
+
+ import "github.com/PocketMiner92/magic-alpaca/server/world" 
+
+  
+
+ // PumpkinPie is a food item that can be eaten by the player. 
+
+ type PumpkinPie struct { 
+
+         defaultFood 
+
+ } 
+
+  
+
+ // Consume ... 
+
+ func (PumpkinPie) Consume(_ *world.World, c Consumer) Stack { 
+
+         c.Saturate(8, 4.8) 
+
+         return Stack{} 
+
+ } 
+
+  
+
+ // EncodeItem ... 
+
+ func (PumpkinPie) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:pumpkin_pie", 0 
+
+ }
