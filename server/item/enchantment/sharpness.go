c7f9fb47c0b5b3917fac6a501348b8e654925df9
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 17:19:31 +0200

    Create sharpness.go

---
+++ server/item/enchantment/sharpness.go
@@ -0,0 +1,59 @@
+ 
+
+ package enchantment 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+ ) 
+
+  
+
+ // Sharpness is an enchantment applied to a sword or axe that increases melee damage. 
+
+ type Sharpness struct{} 
+
+  
+
+ // Addend returns the additional damage when attacking with sharpness. 
+
+ func (e Sharpness) Addend(level int) float64 { 
+
+         return float64(level) * 1.25 
+
+ } 
+
+  
+
+ // Name ... 
+
+ func (e Sharpness) Name() string { 
+
+         return "Sharpness" 
+
+ } 
+
+  
+
+ // MaxLevel ... 
+
+ func (e Sharpness) MaxLevel() int { 
+
+         return 4 
+
+ } 
+
+  
+
+ // CompatibleWith ... 
+
+ func (e Sharpness) CompatibleWith(s item.Stack) bool { 
+
+         t, ok := s.Item().(item.Tool) 
+
+         return ok && (t.ToolType() == item.TypeSword || t.ToolType() == item.TypeAxe) 
+
+ }
