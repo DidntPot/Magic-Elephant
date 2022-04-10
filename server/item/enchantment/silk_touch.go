72406eff6bacdd2118db14b7f43f5cd8453c47eb
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 17:20:25 +0200

    Create silk_touch.go

---
+++ server/item/enchantment/silk_touch.go
@@ -0,0 +1,51 @@
+ 
+
+ package enchantment 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+ ) 
+
+  
+
+ // SilkTouch is an enchantment that allows many blocks to drop themselves instead of their usual items when mined. 
+
+ type SilkTouch struct{} 
+
+  
+
+ // Name ... 
+
+ func (e SilkTouch) Name() string { 
+
+         return "Silk Touch" 
+
+ } 
+
+  
+
+ // MaxLevel ... 
+
+ func (e SilkTouch) MaxLevel() int { 
+
+         return 1 
+
+ } 
+
+  
+
+ // CompatibleWith ... 
+
+ func (e SilkTouch) CompatibleWith(s item.Stack) bool { 
+
+         t, ok := s.Item().(item.Tool) 
+
+         //TODO: Fortune 
+
+         return ok && (t.ToolType() != item.TypeSword && t.ToolType() != item.TypeNone) 
+
+ }
