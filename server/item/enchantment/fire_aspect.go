400986e663fa7364b5fc15bf1a2dbea5ae2cd2ef
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 17:10:51 +0200

    Create fire_aspect.go

---
+++ server/item/enchantment/fire_aspect.go
@@ -0,0 +1,61 @@
+ 
+
+ package enchantment 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+         "time" 
+
+ ) 
+
+  
+
+ // FireAspect is a sword enchantment that sets the target on fire. 
+
+ type FireAspect struct{} 
+
+  
+
+ // Duration returns how long the fire from fire aspect will last. 
+
+ func (e FireAspect) Duration(lvl int) time.Duration { 
+
+         return time.Second * 4 * time.Duration(lvl) 
+
+ } 
+
+  
+
+ // Name ... 
+
+ func (e FireAspect) Name() string { 
+
+         return "Fire Aspect" 
+
+ } 
+
+  
+
+ // MaxLevel ... 
+
+ func (e FireAspect) MaxLevel() int { 
+
+         return 2 
+
+ } 
+
+  
+
+ // CompatibleWith ... 
+
+ func (e FireAspect) CompatibleWith(s item.Stack) bool { 
+
+         t, ok := s.Item().(item.Tool) 
+
+         return ok && t.ToolType() == item.TypeSword 
+
+ }
