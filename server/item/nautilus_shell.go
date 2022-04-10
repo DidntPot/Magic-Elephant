f70fa950c42e6ab6d7ea39dbbe3be4233a634ef0
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 07:59:04 +0200

    Create nautilus_shell.go

---
+++ server/item/nautilus_shell.go
@@ -0,0 +1,19 @@
+ 
+
+ package item 
+
+  
+
+ // NautilusShell is an item that is used for crafting conduits. 
+
+ type NautilusShell struct{} 
+
+  
+
+ // EncodeItem ... 
+
+ func (NautilusShell) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:nautilus_shell", 0 
+
+ }
