eaf27399fa490885f5edbeb23dfe8864b5fcd2b8
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 12:12:07 +0200

    Update Spider_Eye

---
+++ server/item/spider_eye.go
@@ -0,0 +1,49 @@
+ 
+
+ package item 
+
+  
+
+ import ( 
+
+         "github.com/PocketMiner92/magic-alpaca/server/entity/effect" 
+
+         "github.com/PocketMiner92/magic-alpaca/server/world" 
+
+         "time" 
+
+ ) 
+
+  
+
+ // SpiderEye is a poisonous food and brewing item. 
+
+ type SpiderEye struct { 
+
+         defaultFood 
+
+ } 
+
+  
+
+ // Consume ... 
+
+ func (SpiderEye) Consume(_ *world.World, c Consumer) Stack { 
+
+         c.Saturate(2, 3.2) 
+
+         c.AddEffect(effect.New(effect.Poison{}, 1, time.Second*5)) 
+
+         return Stack{} 
+
+ } 
+
+  
+
+ // EncodeItem ... 
+
+ func (SpiderEye) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:spider_eye", 0 
+
+ }
