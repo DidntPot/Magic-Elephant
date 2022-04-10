267de894f335b69fd99947d1aa813720116abbd5
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 17:53:39 +0200

    Create handler_block_pick_request.go

---
+++ server/session/handler_block_pick_request.go
@@ -0,0 +1,33 @@
+ 
+
+ package session 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/block/cube" 
+
+         "github.com/sandertv/gophertunnel/minecraft/protocol/packet" 
+
+ ) 
+
+  
+
+ // BlockPickRequestHandler handles the BlockPickRequest packet. 
+
+ type BlockPickRequestHandler struct{} 
+
+  
+
+ // Handle ... 
+
+ func (b BlockPickRequestHandler) Handle(p packet.Packet, s *Session) error { 
+
+         pk := p.(*packet.BlockPickRequest) 
+
+         s.c.PickBlock(cube.Pos{int(pk.Position.X()), int(pk.Position.Y()), int(pk.Position.Z())}) 
+
+         return nil 
+
+ }
