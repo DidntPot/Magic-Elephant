efb156c812b3a0570f37ec35a60b91d705f86b17
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 17:08:52 +0200

    Create efficiency.go

---
+++ server/item/enchantment/efficiency.go
@@ -0,0 +1,59 @@
+ 
+
+ package enchantment 
+
+  
+
+ import ( 
+
+         "github.com/Pocketminer92/magic-alpaca/server/item" 
+
+ ) 
+
+  
+
+ // Efficiency is an enchantment that increases mining speed. 
+
+ type Efficiency struct{} 
+
+  
+
+ // Addend returns the mining speed addend from efficiency. 
+
+ func (e Efficiency) Addend(level int) float64 { 
+
+         return float64(level*level + 1) 
+
+ } 
+
+  
+
+ // Name ... 
+
+ func (e Efficiency) Name() string { 
+
+         return "Efficiency" 
+
+ } 
+
+  
+
+ // MaxLevel ... 
+
+ func (e Efficiency) MaxLevel() int { 
+
+         return 5 
+
+ } 
+
+  
+
+ // CompatibleWith ... 
+
+ func (e Efficiency) CompatibleWith(s item.Stack) bool { 
+
+         t, ok := s.Item().(item.Tool) 
+
+         return ok && t.ToolType() == item.TypePickaxe 
+
+ }
