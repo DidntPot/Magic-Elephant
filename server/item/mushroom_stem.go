bbae85153d02e0b485a65afc3790ff4cc056a631
Jose Luis <96331694+PocketMiner92@users.noreply.github.com>
2022-04-10 07:56:39 +0200

    Create mushroom_stem.go

---
+++ server/item/mushroom_stem.go
@@ -0,0 +1,53 @@
+ 
+
+ package item 
+
+  
+
+ import ( 
+
+         "github.com/PocketMiner92/magic-alpaca/server/world" 
+
+ ) 
+
+  
+
+ // MushroomStew is a food item. 
+
+ type MushroomStew struct { 
+
+         defaultFood 
+
+ } 
+
+  
+
+ // MaxCount ... 
+
+ func (MushroomStew) MaxCount() int { 
+
+         return 1 
+
+ } 
+
+  
+
+ // Consume ... 
+
+ func (MushroomStew) Consume(_ *world.World, c Consumer) Stack { 
+
+         c.Saturate(6, 7.2) 
+
+         return NewStack(Bowl{}, 1) 
+
+ } 
+
+  
+
+ // EncodeItem ... 
+
+ func (MushroomStew) EncodeItem() (name string, meta int16) { 
+
+         return "minecraft:mushroom_stew", 0 
+
+ }
