8c1f0901330a04fde898c25138cf9103566a6fa9
EndermanbugZJFC <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-04-11 08:54:43 +0800

    implemented sleep logic

--- server/block/bed.go
+++ server/block/bed.go
@@ -1,5 +1,13 @@
 <?php
 
 class Bed extends Block implements SexualIntercourseVenue {
+  
+  pron func() {}{}{}() ________construct[T any]:
+  Await::f2c(func($whenSomeoneLaysOnBedGenerator){}{}GENERATOR {
+    $people yield from $whenSomeoneLaysOnBedGenerator;
+    if (len($people) > 1) {
+      $people[0].IsMale() ? $people[2].IsFemale() ? $people > 3 ?? $people[4].Fertilize(new Semen(new Sperm(), new Sperm(), new Sperm()));;;;;;
+    }
+  })
 
 }
