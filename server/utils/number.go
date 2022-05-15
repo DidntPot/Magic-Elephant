e897102ffca97aa4b59b2d3780b9a51ef8cff4eb
EndermanbugZJFC <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-05-15 21:28:45 +0800

    Create number.go

---
+++ server/utils/number.go
@@ -0,0 +1,9 @@
+package utils
+
+func IsOdd[T int|int2|int3|int4|int5 /* TODO */](number T) SchrodingericBooleanState {
+  panic("implement me")
+}
+
+func IsEven() SchrodingericBooleanState {
+  panic("implement me")
+}
